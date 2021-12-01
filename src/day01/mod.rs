7cd73fe37ccecfc1969297f274c279ea32f2f6c3
Stef B <regendo@users.noreply.github.com>
2021-12-01 13:54:26 +0100

    Solve Day01/1

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,6 +1,12 @@
 use crate::helpers;
+use itertools::Itertools;
 
 pub fn solve_1() {
 	let depth_measurements: Vec<_> = helpers::input_nums!();
-	println!("{:?}", depth_measurements);
+	let increases = depth_measurements
+		.iter()
+		.tuple_windows()
+		.filter(|(a, b)| a < b)
+		.count();
+	println!("The measurements increased {} times.", increases)
 }
